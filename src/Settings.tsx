8043f419dc70207b28165837399368de3acaba33
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-07-09 10:43:12 +0630

    ui only

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,8 +1,95 @@
 const Settings = () => {
   return (
     <main className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-5xl font-extrabold">Setting Page</h1>
-      <h2 className="mt-4">This is where the new users will be added.</h2>
+      <section className="w-full h-full p-6 mx-auto bg-slate-200 dark:bg-gray-800">
+        <h1 className="text-xl font-bold text-black capitalize dark:text-sky-400">
+          Add New Employee
+        </h1>
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label className="text-black dark:text-gray-200">ID</label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label className="text-black dark:text-gray-200">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label className="text-black dark:text-gray-200">Last Name</label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label className="text-black dark:text-gray-200">Age</label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label className="text-black dark:text-gray-200">Position</label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label className="text-black dark:text-gray-200">Image</label>
+              <div className="flex justify-center px-6 pt-5 pb-6 mt-2 border-2 border-gray-300 border-dashed rounded-md">
+                <div className="space-y-1 text-center">
+                  <svg
+                    className="w-12 h-12 mx-auto text-indigo-500 dark:text-sky-400"
+                    stroke="currentColor"
+                    fill="currentcolor"
+                    viewBox="0 0 48 48"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <div className="flex items-center text-sm text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative p-1 font-medium text-indigo-600 bg-white rounded-md cursor-pointer dark:bg-black dark:text-sky-500 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                    >
+                      <span className="">Upload a file</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                      />
+                    </label>
+                    <p className="pl-1 text-black">or drag and drop</p>
+                  </div>
+                  <p className="text-xs text-black">PNG, JPG, GIF up to 10MB</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-end py-6">
+            <button className="px-6 py-2 font-semibold leading-5 text-black transition-colors duration-200 transform rounded-md bg-violet-400 hover:bg-violet-500 focus:outline-none focus:bg-gray-600 dark:bg-sky-400 dark:hover:bg-sky-500">
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
     </main>
   );
 };
